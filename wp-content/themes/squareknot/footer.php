74bfb74f5ae7addf3eb438b078386b53c7cbb03a
Jason Rappaport <jason@squareknot.com>
2013-03-20 06:43:39 -0400

    Updated Wordpress.

--- wp-content/themes/squareknot/footer.php
+++
@@ -1,7 +0,0 @@
-		<?php next_posts_link('<div class="button alignleft" style="margin-left:0;">&laquo; Older Stuff</div>'); ?>
-		<?php previous_posts_link('<div class="button alignleft">Newer Stuff &raquo;</div>'); ?>
-	</div> <!-- /content -->
-</div> <!-- /content-wrap -->
-<?php wp_footer(); ?>
-</body>
-</html>
