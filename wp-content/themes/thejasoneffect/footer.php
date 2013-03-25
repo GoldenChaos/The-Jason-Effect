4002da595a19d0433d7ac44c5a1af7df9c738b9b
Jason Rappaport <blog@thejasoneffect.net>
2013-03-25 19:35:24 -0400

    Looks like I forgot to commit this.

--- wp-content/themes/thejasoneffect/footer.php
+++ wp-content/themes/thejasoneffect/footer.php
@@ -1,9 +1,12 @@
 		<?php if(is_single() || is_page()): ?>
 			<div class="comment-body" id="footer">
-				<?php if(is_single()) {
-					previous_post_link('<div class="post-nav alignleft" style="margin-left:0;">&laquo; %link</div>');
-					next_post_link('<div class="post-nav alignleft">%link &raquo;</div>');
-				} ?>
+				<!-- Hiding this for now since it breaks the mobile layout and in general isn't pretty.
+					<?php if(is_single()) {
+						previous_post_link('<div class="post-nav alignleft" style="margin-left:0;">&laquo; %link</div>');
+						next_post_link('<div class="post-nav alignleft">%link &raquo;</div>');
+						}
+					?>
+				-->
 				<span id="copyright">Copyright &copy; 2005-2013 Jason Rappaport</span>
 				<div class="clear"></div>
 			</div>
